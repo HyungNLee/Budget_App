0f87d926b655c6d9fc805ebb92993d083101abc1
Hyung Lee <hyungnaelee@gmail.com>
2018-12-13 14:28:27 -0800

    Restructure component folders. Add SignIn component.

---
+++ src/components/App/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+// Component Imports
+import SignIn from '../SignIn/SignIn';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SignIn/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/SignIn/SignIn.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function SignIn() {
+  return (
+    <div>
+      <input type='text' name='username' placeholder='Username'/>
+      <input type='text' name='password' placeholder='Password'/>
+      <button>**TEST** Sign In</button>
+    </div>
+  );
+}
+
+export default SignIn;
