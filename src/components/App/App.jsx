82e47f01aa190655cc872a543ee6a7ef79999690
Hyung Lee <hyungnaelee@gmail.com>
2018-12-17 08:49:57 -0800

    Add state, will convert to Redux. **Rollback to this commit if Redux gets messed up**.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
+// UUID Library
+import { v4 } from 'uuid';
 // Component Imports
 import SignIn from '../SignIn/SignIn';
 import Main from '../Main/Main';
 
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+      transactionList: {
+        [v4()]: {
+          // categoryId: '7982uus',
+          description: 'Rent for apartment.',
+          payee: 'Oak Hills Apartments',
+          type: 'Expense',
+          amount: 450,
+          // date: '11/12/2017'
+        },
+      },
+
+
+    };
+  }
+
   render() {
     return (
       <div>
         <Switch>
           <Route exact path='/' component={SignIn} />
-          <Route path='/main' component={Main}/>
+          <Route path='/main' component={Main} />
         </Switch>
       </div>
     );
